52f53b60b1ccff66f3d8a825f0a1877885c1787e
yoad <yoadlanger@gmail.com>
2021-11-13 19:03:30 +0000

    changed vpc module to be used from TFC private registry (currently using UI managed workspace)

--- Homework3/environments/dev/vpc/main.tf
+++ Homework3/environments/dev/vpc/main.tf
@@ -6,8 +6,9 @@ data "aws_availability_zones" "available" {
 }
 
 module "vpc" {
-  source               = "../../../modules/vpc"
-  global_name_prefix   = "hw3"
+  source               = "app.terraform.io/yoad-terrafom-cloud/vpc/aws"
+  version              = "0.0.1"
+  global_name_prefix   = "hw4"
   azs                  = data.aws_availability_zones.available.names
   vpc_cidr_block       = var.vpc_cidr_block
   public_subnet_cidrs  = var.public_subnet_cidrs
